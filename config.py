709cde4f8d50e9b72d82681337ca6057975ec854
nakezzo <ivanshemuda2@gmail.com>
2024-10-27 21:42:41 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+channel_id = -1001901008595  #id канала (для уведомлений)
+
+parner_channel = -1001901008595  # id канала для выплат партнерам
+
+admin = 6000217072  #id админстратора (для выплат)
+
+chats = -1001901008595 #id чата
+
+link_chat = 'https://t.me/+XXX' #Ссылка на чат
+
+token = '5742360177:AAHT3RIXQt-xDQJvgTEbyEYSbiLqxHCXJ9Y' #токен бота
+
+username_bot = '@testingnewfarmingbot'
+
+news_link = 'https://t.me/testingnewfarmingbot'
+
+admin_link = 'https://t.me/testingnewfarmingbot'
